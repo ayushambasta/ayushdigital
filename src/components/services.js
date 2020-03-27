59194a5b02179bdb55ceb8d4a1c8257fa4e0126e
Ayush Saurav <theasaurav@gmail.com>
2020-03-28 01:09:47 +0530

    Minor Changes.
    [28/03/2020]

--- src/components/services.js
+++ src/components/services.js
@@ -13,16 +13,16 @@ function Services(){
                <div className="Serviceslist col-12">
                          <ul>
                              <li>
-                         Create end to end digital functionality of your business.</li>
+                         Create <span style={{color: '#c40655'}}>end to end digital functionality</span> of your business.</li>
                          <li>
-                        Create responsive websites that work on a wide range of devices.</li>
+                        Create <span style={{color: '#c40655'}}>responsive websites</span> that work on a wide range of devices.</li>
                         <li>
-                        Assist with production of clean and functional design.</li>
+                        Assist with production of clean and <span style={{color: '#c40655'}}>functional design</span>.</li>
                         <li>
-                        Bridge communication gap between designers and developers.</li>
+                        <span style={{color: '#c40655'}}>Bridge communication gap</span> between designers and developers.</li>
                         <li>
-                        Integrate your website into an easy to use content management system.</li>
-                        <li>I do SEO for different sites, from Portfolio to Blog.</li>
+                        Integrate your website into an easy to use <span style={{color: '#c40655'}}>content management system</span>.</li>
+                        <li>I do <span style={{color: '#c40655'}}>SEO</span> for different sites, from Portfolio to Blog.</li>
                         </ul>
                 </div>
 
